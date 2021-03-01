e11ba488301ea6becc52266ecb629eb1243b723a
Valtid Caushi <valtido@hotmail.com>
2021-03-01 15:26:20 +0000

    Update BarcodeScannerComponent.tsx
    change from png to jpg should be less resource-intensive.  as the captured images, do not rely on background transparancy.

--- src/BarcodeScannerComponent.tsx
+++ src/BarcodeScannerComponent.tsx
@@ -37,7 +37,7 @@ const BarcodeScannerComponent = ({
       width={width}
       height={height}
       ref={webcamRef}
-      screenshotFormat="image/png"
+      screenshotFormat="image/jpeg"
       videoConstraints={{
         facingMode: 'environment'
       }}
